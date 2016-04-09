3365e87e3920c8cff396e09fa152d77e0056b9d0
caoyi <caoyi0905@163.com>
2016-04-09 14:11:00 +0800

    fix some intentions
    fix some intentions

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 'use strict';
 var CFB = require('cfb');
+
 module.exports = function (buf) {
-	if (!buf || buf.length < 8) {
-		return false;
-	}
-    var cfb = CFB.parse(buf);
-    var WordDocument=cfb.find('WordDocument');
-    if(!WordDocument && typeof(WordDocument)!="undefined" && WordDocument!==0){
-        //can't find WordDocument
+    if (!buf || buf.length < 8) {
         return false;
     }
-    var wIdent=WordDocument.content.readUInt16LE(0);
-    return wIdent==0xa5ec;
+    
+    try{
+        var cfb = CFB.parse(buf);
+        var doc = cfb.find('WordDocument');
+        if (!doc) {
+            return false;
+        }
+        
+        return doc.content.readUInt16LE(0) === 0xa5ec;
+    }catch(e){
+        return false;
+    }
 };
--- test/test.js
+++ test/test.js
@@ -2,13 +2,27 @@
 var path = require('path');
 var test = require('ava');
 var isDoc = require('../');
-var read = require('fs').readFileSync;
+var read = require('fs').readFile;
 test(function (t) {
-	t.plan(3);
-    var buf=path.join(__dirname, 'fixtures/test.doc');
-    var buf2=path.join(__dirname, 'fixtures/test.xls');
-    var buf3=path.join(__dirname, 'fixtures/test.ppt');
-    t.assert(isDoc(read(buf)));
-    t.assert(!isDoc(read(buf2)));
-    t.assert(!isDoc(read(buf3)));
+    t.plan(8);
+    const file1 = path.join(__dirname, 'fixtures/test.doc');
+    const file2 = path.join(__dirname, 'fixtures/test.xls');
+    const file3 = path.join(__dirname, 'fixtures/test.ppt');
+    const file4 = path.join(__dirname, 'fixtures/test.txt');
+    read(file1, function (err, data) {
+        t.assert(!err, err);
+        t.assert(isDoc(data));
+    });
+    read(file2, function (err, data) {
+        t.assert(!err, err);
+        t.assert(!isDoc(data));
+    });
+    read(file3, function (err, data) {
+        t.assert(!err, err);
+        t.assert(!isDoc(data));
+    });
+    read(file4, function (err, data) {
+        t.assert(!err, err);
+        t.assert(!isDoc(data));
+    });
 });
